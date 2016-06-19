055bae8663ab03ae41f60f3186999a4f0981b5ae
Dreamer206602 <boobooMX@163.com>
2016-06-19 18:48:19 +0800

    add some files

---
+++ app/src/main/java/com/mx/booboo/adapter/JokePicAdapter.java
@@ -0,0 +1,76 @@
+package com.mx.booboo.adapter;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mx.booboo.R;
+import com.mx.booboo.mvp.Bean.JokePicBean;
+import com.mx.booboo.utils.ImageLoaderUtils;
+import com.mx.booboo.utils.UIUtils;
+
+import java.util.List;
+
+import butterknife.Bind;
+
+/**
+ * Created by Administrator on 2016/6/19.
+ */
+public class JokePicAdapter extends BaseRecyclerViewAdapter<JokePicBean.JokePicInfo> {
+
+
+public JokePicAdapter(List<JokePicBean.JokePicInfo> datas) {
+        super(datas);
+        }
+
+@Override
+protected void onBind(RecyclerView.ViewHolder holder, int position, JokePicBean.JokePicInfo data) {
+        if (holder instanceof ViewHolder) {
+        ((ViewHolder) holder).setData(data);
+        }
+        }
+
+@Override
+protected BaseRecyclerViewHolder onCreate(ViewGroup parent, int viewType) {
+
+        if (viewType == TYPE_ITEM) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.joke_pic_list_item, parent, false);
+        return new ViewHolder(view);
+
+        } else if (viewType == TYPE_FOOTER) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_foot, parent, false);
+        return new BaseRecyclerViewHolder(view);
+        }
+
+        return null;
+        }
+
+@SuppressWarnings("unused")
+class ViewHolder extends BaseRecyclerViewHolder {
+
+    @Bind(R.id.tv_title)
+    TextView tvTitle;
+    @Bind(R.id.imageView)
+    ImageView image;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void setData(@NonNull JokePicBean.JokePicInfo data) {
+        super.setData(data);
+        tvTitle.setText(data.getTitle());
+        ImageLoaderUtils.display(UIUtils.getContext(), image, data.getImg());
+    }
+}
+}
+
--- app/src/main/java/com/mx/booboo/fragment/JokePicFragment.java
+++ app/src/main/java/com/mx/booboo/fragment/JokePicFragment.java
@@ -1,28 +1,153 @@
 package com.mx.booboo.fragment;
 
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.mx.booboo.R;
+import com.mx.booboo.adapter.JokePicAdapter;
+import com.mx.booboo.constant.Constant;
+import com.mx.booboo.mvp.Bean.JokePicBean;
+import com.mx.booboo.mvp.presenter.BasePresenter;
+import com.mx.booboo.mvp.presenter.JokePicPresenterImpl;
+import com.mx.booboo.mvp.view.BaseView;
+import com.mx.booboo.utils.LogUtils;
 import com.mx.booboo.utils.UIUtils;
+import com.mx.booboo.widget.MyRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class JokePicFragment extends BaseFragment {
+public class JokePicFragment extends BaseFragment implements BaseView.JokePicView, SwipeRefreshLayout.OnRefreshListener, MyRecyclerView.LoadingData {
+
+    private BasePresenter.JokePicPresenter mJokePicPresenter;
+
+    @Bind(R.id.recyclerView)
+    MyRecyclerView mRecyclerView;
+    @Bind(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private JokePicAdapter mJokePicAdapter;
+    private List<JokePicBean.JokePicInfo>mInfos;
+    private static  int page =1;
+
+    View ret;
+
 
     @Override
     protected View initView() {
-        return View.inflate(UIUtils.getActivity(),R.layout.fragment_joke_pic,null);
+
+        if (ret == null) {
+        ret=View.inflate(UIUtils.getActivity(), R.layout.fragment_joke_pic, null);
+    }
+        return ret;
     }
 
     @Override
     protected void initData() {
+        mJokePicPresenter=new JokePicPresenterImpl(this);
+
+        mInfos=new ArrayList<>();
+        mJokePicAdapter=new JokePicAdapter(mInfos);
+
+
+        mSwipeRefreshLayout.setColorSchemeColors(
+                UIUtils.getColor(R.color.colorAccent),
+                UIUtils.getColor(R.color.colorPrimary)
+        );
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLoadingData(this);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(Constant.RECYCLERVIEW_LINEAR, LinearLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(mJokePicAdapter);
+
+
+        Observable.timer(100, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        onRefresh();
+                       mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                });
 
     }
 
+
+    @Override
+    public void setData(List<JokePicBean.JokePicInfo> datas) {
+        LogUtils.d("JokePic",datas.get(0).getImg());
+        mJokePicAdapter.addAll(datas);
+    }
+
+    @Override
+    public void netWorkError() {
+        Toast(UIUtils.getString(R.string.network_error));
+    }
+
+    @Override
+    public void hideProgress() {
+        mSwipeRefreshLayout.setRefreshing(false);
+//        if(mSwipeRefreshLayout.isRefreshing()){
+//            mSwipeRefreshLayout.setRefreshing(false);
+//        }
+
+
+    }
+
+    @Override
+    public void showProgress() {
+        mSwipeRefreshLayout.setRefreshing(true);
+//        if(!mSwipeRefreshLayout.isRefreshing())
+//        {
+//            mSwipeRefreshLayout.setRefreshing(true);
+//        }
+
+
+    }
+
+    @Override
+    public void showFoot() {
+        mJokePicAdapter.isShowFooter(true);
+    }
+
+    @Override
+    public void hideFoot() {
+        mJokePicAdapter.isShowFooter(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        mJokePicAdapter.removeAll();
+        page=1;
+        mJokePicPresenter.requestNetWork(page);
+
+
+    }
+
+    @Override
+    public void onLoadMore() {
+        if(!mSwipeRefreshLayout.isRefreshing()) {
+            ++page;
+            mJokePicPresenter.requestNetWork(page);
+        }
+
+    }
 }
